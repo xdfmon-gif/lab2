b1e270b3792129f9f575cdde725147ff072f83dc
KeatSuan Yeoh <ssssuan88.25@ichat.sp.edu.sg>
2026-05-07 11:02:46 +0800

    modified calc_bmi() to return integer

--- bmi.py
+++ bmi.py
@@ -1,7 +1,17 @@
 def calculate_bmi(height, weight):
     print("Height = " + str(height))
     print("Weight = " + str(weight))
+    
     bmi = weight / (height * height)
     print("BMI = " + str(bmi))
+    
+
+    if bmi < 18.5:
+        print("Classification = Under Weight")
+    elif 18.5 <= bmi <= 25.0:
+        print("Classification = Normal Weight")
+    elif bmi > 25.0:
+        print("Classification = Over Weight")
+
 
 calculate_bmi(weight=57, height=1.73)
